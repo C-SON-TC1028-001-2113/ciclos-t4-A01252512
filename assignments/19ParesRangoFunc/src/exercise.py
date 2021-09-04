09814a2659ac824eefa6e8dddfc7f7b2f0318c75
A01252512 <a01252512@itesm.mx>
2021-09-04 06:48:36 +0000

    ParesRangoFunc

--- assignments/19ParesRangoFunc/src/exercise.py
+++ assignments/19ParesRangoFunc/src/exercise.py
@@ -1,6 +1,28 @@
+def pares(v1, v2):
+    cadena = ''
+    pares = False
+
+    for i in range(v1,v2+1):
+        if i % 2 == 0:
+            pares = True
+            print(i)
+            #if i < v2-1:
+                #cadena = cadena + str(i) + '\n'
+            #else:
+                #cadena = cadena + str(i)
+                
+    if pares:
+        return cadena
+    else:
+        print('No hay pares')
 
 def main():
-
+    val1 = int(input('Valor 1: '))
+    val2 = int(input('Valor 2: '))
+    if val2 >= val1:
+        (pares(val1,val2))
+    else:
+        (pares(val2,val1))
 
 if __name__=='__main__':
     main()
